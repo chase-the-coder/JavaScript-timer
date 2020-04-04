4a3e7deabc0afa043f286adc658b3ec9f2a8f1eb
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-04 14:58:34 -0400

    added getters and setters

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ class Timer {
 		this.durationInput = durationInput;
 		this.startButton = startButton;
 		this.pauseButton = pauseButton;
+
 		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
 	}
 
-	start() {
-		console.log('Time to start the timer!');
+	start = () => {
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
+	};
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	tick = () => {
+		this.timeRemaining = this.timeRemaining - 1;
+	};
+
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	set timeRemaining(time) {
+		this.durationInput.value = time;
 	}
 }
 
